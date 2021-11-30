b1bb0aafb92351d0a46391a832b1afdd50c03dab
sefty <seftyyunitasari@gmail.com>
2021-11-30 17:12:02 +0900

    create delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -29,6 +29,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "I have deleted a post"
   end
 
   private
