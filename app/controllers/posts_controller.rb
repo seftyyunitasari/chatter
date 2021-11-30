f5ef3d907c26e6326ee7f51b30038a7fc97514d5
sefty <seftyyunitasari@gmail.com>
2021-11-30 16:50:08 +0900

    create edit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
+    @posts = Post.all
   end
 
   def new
@@ -7,7 +9,34 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(content: params[:post][:content])
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to new_post_path, notice: "I have created a post!"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "I have edited a post"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
